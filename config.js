03f8ea2ed265447c4be547fb49560e8c67f7d538
LMK360 <lmkfail080@gmail.com>
2025-07-03 19:55:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  session_id: "YOUR_SESSION_ID_HERE",
+  session_id: "IK~BelSwBTa#tf8ixO0x-aq6jbydWvqSAu-SFX1OmxteeoDZVyLle58",
   auto_react: true,
   bot_name: "LMK-AGENT002-MD",
   command_prefix: ".",
